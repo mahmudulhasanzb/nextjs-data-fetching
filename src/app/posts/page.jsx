fc3b0049df2ef70720194f77b6436c6adc60467e
mahmudulhasanzb <mahmudulhasankk9@gmail.com>
2026-04-20 12:57:35 +0600

    added data fetching

--- src/app/posts/page.jsx
+++ src/app/posts/page.jsx
@@ -1,8 +1,43 @@
 import React from 'react'
 
-const PostsPage = () => {
+// const getPosts = async () => {
+//   const res = await fetch('https://jsonplaceholder.typicode.com/posts')
+//   return res.json()
+  
+// }
+
+// const getPosts = async () => {
+//   try {
+//     const res = await fetch('https://jsonplaceholder.typicode.com/posts');
+//   return res.json()
+//   }
+//   catch (err) {
+//     throw new Error("Failed to fetch posts")
+//   }
+// }
+
+const getPosts = async () => {
+  const res = await fetch('https://jsonplaceholder.typicode.com/posts');
+
+  if (!res.ok) {
+    throw new Error("Failed to fetch posts")
+  }
+
+  return res.json();
+};
+
+
+const PostsPage = async () => {
+
+  // const res = await fetch('https://jsonplaceholder.typicode.com/posts');
+  // const posts = await res.json()
+
+  const posts = await getPosts();
+  
   return (
-    <div>PostsPage comming soon...</div>
+    <div>PostsPage comming soon...
+      <h2>total posts: {posts.length}</h2>
+    </div>
   )
 }
 
