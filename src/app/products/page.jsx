bd65e4f1b7860ede0419cd2afd8194c4cad3ba24
mahmudulhasanzb <mahmudulhasankk9@gmail.com>
2026-04-20 15:19:01 +0600

    explore a new concept 'cache forece'

---
+++ src/app/products/page.jsx
@@ -0,0 +1,24 @@
+import Product from '@/components/Product';
+import React from 'react'
+
+const getProducts = async () => {
+  const res = await fetch('http://localhost:5000/products', {cache: "force-cache" });
+  return res.json()
+}
+
+const ProductsPage = async () => {
+  const products = await getProducts()
+  return (
+    <div>
+      <h2 className='mt-5 mb-5'>total products length : {products.length} </h2>
+
+      <div className='grid grid-cols-3 gap-4'>
+        {
+          products.map(product => <Product key={product.id} product={product}></Product>)
+        }
+      </div>
+    </div>
+  )
+}
+
+export default ProductsPage
